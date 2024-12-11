628c7f9516dc618f2949b39e6ac29b639b87659b
chloeeyongg <cyong0001@mymail.lausd.net>
2024-12-11 13:13:07 -0800

    done

--- DataScene.java
+++ DataScene.java
@@ -1,11 +1,11 @@
 import org.code.theater.*;
 
 public class DataScene extends Scene {
-    private double[] heightsInMeters;
-    private String[] mountainNames;
-    private String[] ranges;
+  private double[] heightsInMeters;
+  private String[] mountainNames;
+  private String[] ranges;
     private String[] countries;
-    private String[] textColors;
+  private String[] textColors;
     private String[] mountainImages;
 
     private double highestPeak;
@@ -14,12 +14,12 @@ public class DataScene extends Scene {
      * Constructor for initializing the DataScene object with the given data
      */
     public DataScene(double[] heightsInMeters, String[] mountainNames, String[] ranges, String[] countries, String[] colors, String[] images) {
-        this.heightsInMeters = heightsInMeters;
+         this.heightsInMeters = heightsInMeters;
         this.mountainNames = mountainNames;
         this.ranges = ranges;
-        this.countries = countries;
-        this.textColors = colors;
-        this.mountainImages = images;
+         this.countries = countries;
+          this.textColors = colors;
+         this.mountainImages = images;
         calculateHighestPeak();
     }
 
@@ -27,27 +27,28 @@ public class DataScene extends Scene {
      * Calculates the highest peak in the dataset
      */
     private void calculateHighestPeak() {
-        highestPeak = findMax(heightsInMeters);
-    }
-
-    /**
-     * Finds the maximum value
-     */
-    private double findMax(double[] array) {
-        double max = array[0];
-        for (double value : array) {
-            max = Math.max(max, value);
-        }
-        return max;
+      highestPeak = findMax(heightsInMeters);
     }
 
     /**
      * Displays all mountain details 
      */
     public void showAllMountainDetails() {
-        for (int i = 0; i < mountainNames.length; i++) {
-            showMountainDetails(i);
+    for (int i = 0; i < mountainNames.length; i++) {
+        showMountainDetails(i);
         }
+    }
+
+      /**
+     * *Finds the maximum value
+     */
+    private double findMax(double[] array) {
+        double max = array[0];
+        for (double value : array) {
+            max = Math.max(max, value);
+        }
+       
+return max;
     }
 
     /**
@@ -55,64 +56,65 @@ public class DataScene extends Scene {
      */
     private void showMountainDetails(int index) {
         String message = getMessage(index);
-        String image = getImageForMountain(index);
-        drawMountainDetails("Mountain Details", message, mountainNames[index], heightsInMeters[index], ranges[index], countries[index], image, "silver");
+     String image = getImageForMountain(index);
+  drawMountainDetails("Mountain Details", message, mountainNames[index], heightsInMeters[index], ranges[index], countries[index], image, "silver");
     }
 
-    /**
-     * Generates a message for the mountain based on its height
-     */
-    private String getMessage(int index) {
-        if (heightsInMeters[index] == highestPeak) {
-            return "Highest Peak!";
-        } else {
-            return "Impressive Height!";
-        }
-    }
 
     /**
      * Retrieves the image file path for the mountain
      */
     private String getImageForMountain(int index) {
-        return index < mountainImages.length ? mountainImages[index] : "images/mountainImage.jpg";
+   return index < mountainImages.length ? mountainImages[index] : "images/mountainImage.jpg";
     }
 
+    /**
+     * Makes a message for the mountain based on its height
+     */
+    private String getMessage(int index) {
+        if (heightsInMeters[index] == highestPeak) {
+      return "Highest peak!";
+        } else {
+      return "Impressive height!";
+        }
+    }
+
+    /**
+     * Create visual scenes
+     */
+    public void createScene() {
+        for (int i = 0; i < heightsInMeters.length; i++) {
+        createMountainScene(i);
+        }
+    }
+  
     /**
      * Draws detailed information about a mountain
      */
     private void drawMountainDetails(String title, String message, String name, double height, String range, String country, String image, String color) {
         drawImage(image, 0, 0, 450);
-        setFillColor(color);
+       setFillColor(color);
         drawRectangle(0, 0, 410, 50);
         drawRectangle(0, 300, 410, 110);
-        setTextColor("black");
+    setTextColor("black");
         setTextHeight(34);
         drawText(title, 65, 40);
-        setTextHeight(25);
+       setTextHeight(25);
         drawText(message, 20, 325);
         setTextHeight(18);
-        drawText(name, 20, 350);
+       drawText(name, 20, 350);
         drawText("Height: " + Math.round(height) + " m", 20, 375);
         drawText("Range: " + range, 20, 400);
-        drawText("Country: " + country, 20, 425);
+       drawText("Country: " + country, 20, 425);
         playSound("chime.wav");
-        pause(2);
-    }
-
-    /**
-     * Iterates through all mountains to create visual scenes
-     */
-    public void createScene() {
-        for (int i = 0; i < heightsInMeters.length; i++) {
-            createMountainScene(i);
-        }
+      pause(2);
     }
 
     /**
      * Creates a scene for a specific mountain
      */
     private void createMountainScene(int index) {
-        String image = getImageForMountain(index);
-        drawMountainDetails("Mountain Details", getMessage(index), mountainNames[index], heightsInMeters[index], ranges[index], countries[index], image, "silver");
+     String image = getImageForMountain(index);
+      drawMountainDetails("Mountain Details", getMessage(index), mountainNames[index], heightsInMeters[index], ranges[index], countries[index], image, "silver");
     }
 }
--- TheaterRunner.java
+++ TheaterRunner.java
@@ -3,25 +3,15 @@ import org.code.theater.*;
 public class TheaterRunner {
     public static void main(String[] args) {
         /**
-         * Read data from files
+         * Read data from data files
          */
         String[] mountainNames = FileReader.toStringArray("mountainNames.txt");
-        String[] ranges = FileReader.toStringArray("ranges.txt");
+         String[] ranges = FileReader.toStringArray("ranges.txt");
         String[] countries = FileReader.toStringArray("countries.txt");
 
-        // Heights in meters for the mountains
-        double[] heightsInMeters = FileReader.toDoubleArray("heightsInMeters.txt");
+         double[] heightsInMeters = FileReader.toDoubleArray("heightsInMeters.txt");
 
-        String[] colorsArray = {"white", "silver", "gold", "blue", "green"};
-
-        // Images corresponding to mountains
-        String[] mountainImages = {
-            "images-Everest.jpg",      // Mount Everest
-            "images-K2.jpg",           // K2
-            "images-Kangchenjunga.jpg", // Kangchenjunga
-            "images-Lhotse.jpg",       // Lhotse
-            "images-mountainImage.jpg" // Default for others
-        };
+         String[] colorsArray = {"white", "silver", "gold", "blue", "green"};
 
         /**
          * Create a DataScene object
