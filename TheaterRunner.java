bed80db647c9d588d57d5591dac3c5759d84b589
chloeeyongg <cyong0001@mymail.lausd.net>
2024-12-12 12:41:11 -0800

    done

--- TheaterRunner.java
+++ TheaterRunner.java
@@ -1,28 +1,23 @@
 import org.code.theater.*;
 
 public class TheaterRunner {
-    public static void main(String[] args) {
-        /**
-         * Read data from data files
-         */
-        String[] mountainNames = FileReader.toStringArray("mountainNames.txt");
-         String[] ranges = FileReader.toStringArray("ranges.txt");
-        String[] countries = FileReader.toStringArray("countries.txt");
+  public static void main(String[] args) {
 
-         double[] heightsInMeters = FileReader.toDoubleArray("heightsInMeters.txt");
 
-         String[] colorsArray = {"white", "silver", "gold", "blue", "green"};
 
-        /**
-         * Create a DataScene object
-         */
-        DataScene mountainScene = new DataScene(heightsInMeters, mountainNames, ranges, countries, colorsArray);
 
-        // Display all mountain details sequentially
-        mountainScene.showAllMountainDetails();
 
-        // Create and play scenes
-        mountainScene.createScene();
-        Theater.playScenes(mountainScene);
-    }
+
+
+    // Instantiate a DataScene object to play in the Theater
+    DataScene scene = new DataScene();
+    
+    // Call any DataScene methods to diplay
+    scene.drawScene();
+    
+    // Play the DataScene in the theater
+    Theater.playScenes(scene);
+
+    
+  }
 }
